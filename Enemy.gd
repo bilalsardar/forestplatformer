28f3a8a68e2c13b970b79f0bac02a9b84ae88f93
Bilal Sardar <m.bilalsardar@hotmail.com>
2019-07-03 20:29:31 +0500

    enemy movement complete
    enemy movement, detecting wall with is_on_wall()
    detecting platform ledge with RayCast2D. changing the direction and position with is_on_wall() ifcheck.

--- Enemy.gd
+++ Enemy.gd
@@ -7,6 +7,7 @@ const FLOOR = Vector2(0,-1)
 
 var velocity = Vector2()
 var direction = 1 #we keeping default to right
+var flip_sprite = false
 
 
 func _ready():
@@ -15,13 +16,24 @@ func _ready():
 func _physics_process(delta):
 	velocity.x = SPEED * direction
 	
+	if direction == 1:
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
+		
 	$AnimatedSprite.play("walk")
 	
 	velocity.y +=GRAVITY
 	
 	velocity = move_and_slide(velocity,FLOOR)
 	
+	
 	if is_on_wall():
 		direction = direction * -1
-		$AnimatedSprite.flip_h = true
+		$RayCast2D.position.x *= -1
+	
+	if $RayCast2D.is_colliding() == false:
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
+		
 	
